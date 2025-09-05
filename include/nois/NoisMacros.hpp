51e9ca0f3341b108c34fc86da3ff083b6f1bb1a5
bmqn <18558188+bmqn@users.noreply.github.com>
2025-09-05 19:38:09 +0100

    Implement ScopedNoDenorms for gcc/clang arm64.

--- include/nois/NoisMacros.hpp
+++ include/nois/NoisMacros.hpp
@@ -1,5 +1,29 @@
 #pragma once
 
+#if defined(__x86_64__) || defined(_M_X64)
+#define NOIS_ARCH_X64 1
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define NOIS_ARCH_ARM64 1
+#else
+#error "Unknown architecture"
+#endif
+
+#if defined(_WIN32)
+#define NOIS_TARGET_WINDOWS 1
+#elif defined(__linux__)
+#define NOIS_TARGET_LINUX 1
+#elif defined(__APPLE__)
+#include <TargetConditionals.h>
+#if TARGET_OS_IPHONE
+#define NOIS_TARGET_IOS 1
+#elif TARGET_OS_MAC
+#define NOIS_TARGET_MAC 1
+#else
+#error "Unknown Apple platform"
+#endif
+#endif
+
+
 #define NOIS_INTERFACE_GETTER(_name, _type) \
 	Ref_t<_type> Get##_name() const;
 
