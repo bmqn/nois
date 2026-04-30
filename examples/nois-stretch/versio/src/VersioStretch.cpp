0bc02a6dfb5f63dc4ed9dba14bfd8ebd3ae959bb
bmqn <18558188+bmqn@users.noreply.github.com>
2026-04-30 22:08:26 +0100

    Use umm_malloc to allocate from sdram for new/delete.

--- examples/nois-stretch/versio/src/VersioStretch.cpp
+++ examples/nois-stretch/versio/src/VersioStretch.cpp
@@ -1,5 +1,7 @@
 #include "daisy_versio.h"
 
+#include <umm_malloc.h>
+
 #include <nois/Nois.hpp>
 
 using namespace daisy;
@@ -8,29 +10,40 @@ DaisyVersio hw;
 
 static constexpr size_t kBlockSize = 128;
 
-static float scratch[2 * kBlockSize] = { 0.0f };
+static uint8_t DSY_SDRAM_BSS sdram_heap[64 * 1024 * 1024];
+
+static float scratchIn[2 * kBlockSize] = { 0.0f };
+static float scratchOut[2 * kBlockSize] = { 0.0f };
+
 static float stretchActiveValue = 0.0f;
 static float stretchFactorValue = 0.0f;
 
-static nois::ParameterRegistry<nois::f32_t> registry;
+static nois::Ref_t<nois::ParameterRegistry<nois::f32_t>> registry = nullptr;
 static nois::Ref_t<nois::FloatParameter> stretchActive = nullptr;
 static nois::Ref_t<nois::FloatParameter> stretchFactor = nullptr;
 static nois::Ref_t<nois::TimeStretcher> timeStretcher = nullptr;
 
+void* operator new(size_t size) { return umm_malloc(size); }
+void* operator new[](size_t size) { return umm_malloc(size); }
+void operator delete(void* ptr) noexcept { umm_free(ptr); }
+void operator delete[](void* ptr) noexcept { umm_free(ptr); }
+void operator delete(void* ptr, size_t) noexcept { umm_free(ptr); }
+void operator delete[](void* ptr, size_t) noexcept { umm_free(ptr); }
+
 void callback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	registry.Prepare(size, hw.AudioSampleRate());
+	registry->Prepare(size, hw.AudioSampleRate());
 	timeStretcher->Prepare(size, 2, hw.AudioSampleRate());
 
-	std::memcpy(scratch             , in[0], size * sizeof(float));
-	std::memcpy(scratch + kBlockSize, in[1], size * sizeof(float));
+	std::memcpy(scratchIn             , in[0], size * sizeof(float));
+	std::memcpy(scratchIn + kBlockSize, in[1], size * sizeof(float));
 
-	nois::ConstFloatBufferView inView(scratch, size, 2);
-	nois::FloatBufferView outView(scratch, size, 2);
+	nois::ConstFloatBufferView inView(scratchIn, size, 2);
+	nois::FloatBufferView outView(scratchOut, size, 2);
 	timeStretcher->Process(inView, outView);
 
-	std::memcpy(out[0], scratch             , size * sizeof(float));
-	std::memcpy(out[1], scratch + kBlockSize, size * sizeof(float));
+	std::memcpy(out[0], scratchOut             , size * sizeof(float));
+	std::memcpy(out[1], scratchOut + kBlockSize, size * sizeof(float));
 }
 
 int main(void)
@@ -38,13 +51,17 @@ int main(void)
 	hw.Init();
 	hw.SetAudioBlockSize(kBlockSize);
 
-	stretchActive = registry.CreateBinder(
+	umm_init_heap(sdram_heap, sizeof(sdram_heap));
+
+	registry = nois::MakeRef<nois::ParameterRegistry<nois::f32_t>>();
+
+	stretchActive = registry->CreateBinder(
 		[](nois::count_t)
 		{
 			return stretchActiveValue;
 		});
 
-	stretchFactor = registry.CreateBinder(
+	stretchFactor = registry->CreateBinder(
 		[](nois::count_t)
 		{
 			return 1.0f + stretchFactorValue * (16.0f - 1.0f);
@@ -60,8 +77,6 @@ int main(void)
 	while (true)
 	{
 		hw.ProcessAnalogControls();
-
-		hw.UpdateExample();
 
 		stretchActiveValue = hw.SwitchPressed() ? 1.0f : 0.0f;
 		stretchFactorValue = hw.GetKnobValue(DaisyVersio::KNOB_0);
