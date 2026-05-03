ec904acb190734c78a5d2c88c78fba9820033069
bmqn <18558188+bmqn@users.noreply.github.com>
2026-05-03 20:49:03 +0100

    Add Makefile build for Versio firmware.
    The CMake build doesn't work.
    We're likely overriding compiler flags.
    I'll come back to this another time.

--- examples/nois-stretch/versio/src/VersioStretch.cpp
+++ examples/nois-stretch/versio/src/VersioStretch.cpp
@@ -1,96 +1,104 @@
 #include "daisy_versio.h"
-
 #include <umm_malloc.h>
-
 #include <nois/Nois.hpp>
 
 using namespace daisy;
 
 DaisyVersio hw;
 
-static constexpr size_t kBlockSize = 128;
+static uint8_t DSY_SDRAM_BSS sdramHeap[32 * 1024 * 1024];
+void* UMM_MALLOC_CFG_HEAP_ADDR = &sdramHeap;
+size_t UMM_MALLOC_CFG_HEAP_SIZE = sizeof(sdramHeap);
 
-static uint8_t DSY_SDRAM_BSS sdram_heap[64 * 1024 * 1024];
-
-static float scratchIn[2 * kBlockSize] = { 0.0f };
-static float scratchOut[2 * kBlockSize] = { 0.0f };
+static float scratchIn[2 * 128];
+static float scratchOut[2 * 128];
 
 static float stretchActiveValue = 0.0f;
 static float stretchFactorValue = 0.0f;
 static float grainSizeValue = 0.0f;
+static float grainBlendValue = 0.5f;
 
-static nois::Ref_t<nois::ParameterRegistry<nois::f32_t>> registry = nullptr;
-static nois::Ref_t<nois::FloatParameter> stretchActive = nullptr;
-static nois::Ref_t<nois::FloatParameter> stretchFactor = nullptr;
-static nois::Ref_t<nois::FloatParameter> grainSize = nullptr;
+static nois::Ref_t<nois::Registry<nois::f32_t>> registry = nullptr;
 static nois::Ref_t<nois::TimeStretcher> timeStretcher = nullptr;
 
-void* operator new(size_t size) { return umm_malloc(size); }
-void* operator new[](size_t size) { return umm_malloc(size); }
-void operator delete(void* ptr) noexcept { umm_free(ptr); }
-void operator delete[](void* ptr) noexcept { umm_free(ptr); }
-void operator delete(void* ptr, size_t) noexcept { umm_free(ptr); }
-void operator delete[](void* ptr, size_t) noexcept { umm_free(ptr); }
-
-void callback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+static void callback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
+	hw.ProcessAllControls();
+	hw.tap.Debounce();
+
+	stretchActiveValue = hw.SwitchPressed() ? 1.0f : 0.0f;
+	stretchFactorValue = hw.GetKnobValue(DaisyVersio::KNOB_0);
+	grainSizeValue = hw.GetKnobValue(DaisyVersio::KNOB_1);
+	grainBlendValue = hw.GetKnobValue(DaisyVersio::KNOB_2);
+
 	registry->Prepare(size, hw.AudioSampleRate());
 	timeStretcher->Prepare(size, 2, hw.AudioSampleRate());
 
-	std::memcpy(scratchIn             , in[0], size * sizeof(float));
-	std::memcpy(scratchIn + kBlockSize, in[1], size * sizeof(float));
+	for (size_t i = 0; i < size; ++i)
+	{
+		scratchIn[i] = IN_L[i];
+		scratchIn[i + size] = IN_R[i];
+	}
 
 	nois::ConstFloatBufferView inView(scratchIn, size, 2);
 	nois::FloatBufferView outView(scratchOut, size, 2);
 	timeStretcher->Process(inView, outView);
 
-	std::memcpy(out[0], scratchOut             , size * sizeof(float));
-	std::memcpy(out[1], scratchOut + kBlockSize, size * sizeof(float));
+	for (size_t i = 0; i < size; ++i)
+	{
+		OUT_L[i] = scratchOut[i];
+		OUT_R[i] = scratchOut[i + size];
+	}
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.SetAudioBlockSize(kBlockSize);
+	hw.Init(true);
 
-	umm_init_heap(sdram_heap, sizeof(sdram_heap));
+	umm_init();
 
-	registry = nois::MakeRef<nois::ParameterRegistry<nois::f32_t>>();
+	hw.StartAdc();
 
-	stretchActive = registry->CreateBinder(
+	registry = nois::MakeRef<nois::Registry<nois::f32_t>>();
+
+	auto stretchTimeMs = registry->CreateBlockConstant(10000.0f);
+
+	auto stretchActive = registry->CreateBinder(
 		[](nois::count_t)
 		{
 			return stretchActiveValue;
 		});
 
-	stretchFactor = registry->CreateBinder(
+	auto stretchFactor = registry->CreateBinder(
 		[](nois::count_t)
 		{
 			return 1.0f + stretchFactorValue * (16.0f - 1.0f);
 		});
 
-	grainSize = registry->CreateBinder(
+	auto grainSize = registry->CreateBinder(
 		[](nois::count_t)
 		{
 			return 250.0f + grainSizeValue * (3000.0f - 250.0f);
 		});
 
+	auto grainBlend = registry->CreateBinder(
+		[](nois::count_t)
+		{
+			return grainBlendValue / 2.0f;
+		});
+
 	timeStretcher = nois::TimeStretcher::Create();
+	timeStretcher->SetStretchTimeMs(stretchTimeMs);
 	timeStretcher->SetStretchActive(stretchActive);
 	timeStretcher->SetStretchFactor(stretchFactor);
 	timeStretcher->SetGrainSize(grainSize);
+	timeStretcher->SetGrainBlend(grainBlend);
 
 	hw.StartAudio(callback);
-	hw.StartAdc();
 
 	while (true)
 	{
-		hw.ProcessAnalogControls();
-
-		stretchActiveValue = hw.SwitchPressed() ? 1.0f : 0.0f;
-		stretchFactorValue = hw.GetKnobValue(DaisyVersio::KNOB_0);
-		grainSizeValue = hw.GetKnobValue(DaisyVersio::KNOB_1);
-
+		hw.UpdateExample();
 		hw.UpdateLeds();
 	}
 }
