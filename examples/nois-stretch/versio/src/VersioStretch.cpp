7084cadd6da6210fb0abbeeb662f0dabd35842ca
Max Bryars-Mansell <18558188+maxbryarsmansell@users.noreply.github.com>
2026-05-06 21:32:51 +0100

    Implement swing pulses that follow incoming gate pulses.

--- examples/nois-stretch/versio/src/VersioStretch.cpp
+++ examples/nois-stretch/versio/src/VersioStretch.cpp
@@ -13,41 +13,113 @@ size_t UMM_MALLOC_CFG_HEAP_SIZE = sizeof(sdramHeap);
 static float scratchIn[2 * 128];
 static float scratchOut[2 * 128];
 
+static float timeSec = 0.0f;
+
+static bool prevGate = false;
+static float beatSec = 0.0f;
+static float lastBeatSec;
+
+static int swingStepIndex = 0;
+static float pendingSwingTrigger = 0.0f;
+static float swingLedSec = 0.0f;
+
+static float stretchActiveEnvSec = 0.0f;
+
 static float stretchActiveValue = 0.0f;
 static float stretchFactorValue = 0.0f;
 static float grainSizeValue = 0.0f;
 static float grainBlendValue = 0.5f;
 static float grainPhaseIncValue = 1.0f;
 
-static float gateStretchActiveValue = 0.0f;
-
 static nois::Ref_t<nois::Registry<nois::f32_t>> registry = nullptr;
 static nois::Ref_t<nois::TimeStretcher> timeStretcher = nullptr;
+
+enum
+{
+	KNOB_SWING = DaisyVersio::KNOB_6,
+};
+
+static bool IsGatePulse(bool gate)
+{
+	bool pulseGate = gate && !prevGate;
+	prevGate = gate;
+	return pulseGate;
+}
 
 static void callback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
 	hw.ProcessAllControls();
 	hw.tap.Debounce();
 
-	if (gateStretchActiveValue > 0.0f)
+	float deltaSec = static_cast<float>(size) / hw.AudioSampleRate();
+
+	bool gatePulse = IsGatePulse(hw.Gate());
+
+	if (gatePulse)
 	{
-		gateStretchActiveValue -= 1.0f;
+		float edgeDelta = timeSec - lastBeatSec;
+		// smooth estimate (important!)
+		beatSec = 0.9f * beatSec + 0.1f * edgeDelta;
+		lastBeatSec = timeSec;
 	}
-	if (hw.Gate() && gateStretchActiveValue <= 0.0f)
+
+	bool swingPulse = false;
+
+	if (gatePulse)
 	{
-		float stretchActiveProbability = hw.GetKnobValue(DaisyVersio::KNOB_4);
-		if (stretchActiveProbability <= 0.01f)
+		float swing = hw.GetKnobValue(KNOB_SWING);
+		bool isOffBeat = (swingStepIndex % 2 == 1);
+		pendingSwingTrigger = isOffBeat ? beatSec * swing : 0.0f;
+		++swingStepIndex;
+	}
+	if (pendingSwingTrigger >= 0.0f)
+	{
+		pendingSwingTrigger -= deltaSec;
+		if (pendingSwingTrigger <= 0.0f)
 		{
-			gateStretchActiveValue = 0.0f;
-		}
-		else if (Random::GetFloat(0.0f, 1.0f) < stretchActiveProbability)
-		{
-			float stretchActiveEnvelopeLength = hw.GetKnobValue(DaisyVersio::KNOB_5);
-			gateStretchActiveValue = 500.0f * stretchActiveEnvelopeLength;
+			swingPulse = true;
+			pendingSwingTrigger = -1.0f;
 		}
 	}
 
-	stretchActiveValue = (hw.SwitchPressed() || gateStretchActiveValue > 0.0f) ? 1.0f : 0.0f;
+	// If the envelope active
+	if (stretchActiveEnvSec > 0.0f)
+	{
+		stretchActiveEnvSec -= deltaSec;
+	}
+	// If the gate swing is rising edge
+	if (swingPulse)
+	{
+		// Probability of envelope trigger
+		// Offset to account for noise in knob
+		float probability = std::max(0.0f, hw.GetKnobValue(DaisyVersio::KNOB_4) - 0.05f);
+		if (Random::GetFloat(0.0f, 1.0f) < probability)
+		{
+			// Envelope length
+			// Up to 500 ms long
+			float envelopeLength = hw.GetKnobValue(DaisyVersio::KNOB_5);
+			stretchActiveEnvSec = 0.5f * envelopeLength;
+		}
+	}
+
+	if (swingPulse)
+	{
+		swingLedSec = 10.0f / 1000.0f;
+	}
+	if (swingLedSec >= 0.0f)
+	{
+		swingLedSec -= deltaSec;
+		if (swingLedSec <= 0.0f)
+		{
+			swingLedSec = -1.0f;
+		}
+	}
+	hw.SetLed(DaisyVersio::LED_1, 0.0f, swingLedSec >= 0.0f, 0.0f);
+
+	// hw.UpdateExample();
+	hw.UpdateLeds();
+
+	stretchActiveValue = (hw.SwitchPressed() || stretchActiveEnvSec > 0.0f) ? 1.0f : 0.0f;
 	stretchFactorValue = hw.GetKnobValue(DaisyVersio::KNOB_0);
 	grainSizeValue = hw.GetKnobValue(DaisyVersio::KNOB_1);
 	grainBlendValue = hw.GetKnobValue(DaisyVersio::KNOB_2);
@@ -71,6 +143,8 @@ static void callback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
 		OUT_L[i] = scratchOut[i];
 		OUT_R[i] = scratchOut[i + size];
 	}
+
+	timeSec += deltaSec;
 }
 
 int main(void)
@@ -80,6 +154,11 @@ int main(void)
 	// Gate idea is the envelope
 	// Pre-record 10 seconds and then play effect back
 	// Probability for trigger, but manual button press is 100%
+	//
+	// Switch to C then while you hold the button it records and when you let go it stops
+	// Once you have a recording you can go crazy with pitch
+	// Maybe let you record into multiple banks and then switch between them
+	// Three modes: realtime, bank A, and bank C
 
 	hw.Init(true);
 
@@ -135,7 +214,5 @@ int main(void)
 
 	while (true)
 	{
-		hw.UpdateExample();
-		hw.UpdateLeds();
 	}
 }
